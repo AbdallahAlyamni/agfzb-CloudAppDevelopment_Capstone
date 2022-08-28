31cfca3e34d99bd8dd15a741729cd5df6d0f880d
AbdallahAlyamni <AbdallahAlyamni@gmail.com>
2022-08-28 18:36:00 +0200

    - Create Django Proxy Services Of Cloud Functions

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -32,21 +32,46 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-# class CarDealer:
+class CarDealer:
 
-#     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
-#         self.address = address
-#         self.city = city
-#         self.full_name = full_name
-#         self.id = id
-#         self.lat = lat
-#         self.long = long
-#         self.short_name = short_name
-#         self.st = st
-#         self.zip = zip
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        self.address = address
+        self.city = city
+        self.full_name = full_name
+        self.id = id
+        self.lat = lat
+        self.long = long
+        self.short_name = short_name
+        self.st = st
+        self.zip = zip
 
-#     def __str__(self):
-#         return "Dealer name: " + self.full_name
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = None
+        self.id = id
+    
+    #
+    @property            # first decorate the getter method
+    def sentiment(self): # This getter method name is *the* name
+        return self._sentiment
+    #
+    @sentiment.setter    # the property decorates with `.setter` now
+    def sentiment(self, value):   # name, e.g. "attribute", is the same
+        self._sentiment = value   # the "value" name isn't special
+    #
+    @sentiment.deleter     # decorate with `.deleter`
+    def sentiment(self):   # again, the method name is the same
+        del self._sentiment
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,6 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 
 
@@ -30,11 +30,31 @@ def get_dealers_from_cf(url, **kwargs):
     json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
-        dealers = json_result["rows"]
+        dealers = json_result["docList"]
         # For each dealer object
         for dealer in dealers:
             # Get its content in `doc` object
-            dealer_doc = dealer["doc"]
+            dealer_doc = dealer
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+def get_dealers_by_state(url, state):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url,state=state)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["docList"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer
             # Create a CarDealer object with values in `doc` object
             dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
                                    id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
@@ -49,6 +69,25 @@ def get_dealers_from_cf(url, **kwargs):
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
+
+def get_dealer_reviews_from_cf(url, dealerId):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url,dealerId=dealerId)
+    print(json_result)
+    if json_result:
+        # Get the row list in JSON as dealers
+        reviews = json_result["reviews"]
+        # For each dealer object
+        for review in reviews:
+            # Create a CarDealer object with values in `doc` object
+            review_obj = DealerReview(dealership=review["dealership"], name=review["name"], purchase=review["purchase"],
+                                   review=review["review"], purchase_date=review["purchase_date"], car_make=review["car_make"],
+                                   car_model=review["car_model"],
+                                   car_year=review["car_year"], id=review["_id"])
+            results.append(review_obj)
+
+    return results
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
